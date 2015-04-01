ac1a32014c0c23f46f120c995303f7233284755e
Grant Carroll <gcarroll2603@gmail.com>
2015-04-01 19:50:15 -0400

    Added pages for choosing packs and importing sets (sets is open to everyone due to issues with admin import-export settings)

--- SealedSimulator/cardlist/views.py
+++ SealedSimulator/cardlist/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import RequestContext, loader
+from cardlist.models import Card, Set
 
 def pack_select(request):
-    return HttpResponse("Hello World. Welcome to The MtG Sealed Simulator.")
+    card_list = Card.objects.order_by('card_name')
+    template = loader.get_template('cardlist/index.html')
+    context = RequestContext(request, {
+        'card_list': card_list,
+    })
+    return HttpResponse(template.render(context))
 
 def open_packs(request):
     return HttpResponse("This page will contain the cards from your open packs")
 
 def print_selected_cards(request):
     return HttpResponse("This page will contain selected cards in an easy print format")
+    
+def import_set(request, set_name):
+    return HttpResponse("Importing Set: %s." % set_name)
