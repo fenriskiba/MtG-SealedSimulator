0d1284343b466f59c665a1c2d46819b969782313
Grant Carroll <gcarroll2603@gmail.com>
2015-04-02 01:21:08 -0400

    Added basic visual for simple completely random packs

--- SealedSimulator/cardlist/views.py
+++ SealedSimulator/cardlist/views.py
@@ -7,12 +7,23 @@ from time import mktime
 import urllib2
 import json
 import time
+import random
 
 def pack_select(request):
-    card_list = Card.objects.order_by('card_name')
+    pack1 = generatePack("KTK")
+    pack2 = generatePack("KTK")
+    pack3 = generatePack("KTK")
+    pack4 = generatePack("FRF")
+    pack5 = generatePack("FRF")
+    pack6 = generatePack("FRF")
     template = loader.get_template('cardlist/index.html')
     context = RequestContext(request, {
-        'card_list': card_list,
+        'pack1': pack1,
+        'pack2': pack2,
+        'pack3': pack3,
+        'pack4': pack4,
+        'pack5': pack5,
+        'pack6': pack6,
     })
     return HttpResponse(template.render(context))
 
@@ -31,13 +42,15 @@ def import_set(request, set_name):
     })
     return HttpResponse(template.render(context))
 
-#Utility Function 
+#Utility Functions
+
 def getJsonData(set_name):
   json_string = urllib2.urlopen('http://mtgjson.com/json/' + set_name + '.json').read()
   data = json.loads(json_string)
   return data
   
 def addSetToDatabase(set_data):
+    #Add new set
     newSet = Set()
     newSet.set_name = set_data['name']
     newSet.set_abbrev = set_data['code']
@@ -53,6 +66,7 @@ def addSetToDatabase(set_data):
     return
 
 def addCardsToDatabase(set_data):
+    #Add new cards
     for card in set_data['cards']:
         newCard = Card()
         newCard.card_name = card['name']
@@ -61,7 +75,7 @@ def addCardsToDatabase(set_data):
         newCard.set_abbrev = set_data['code']
         newCard.save()
         
-    #Eliminate all duplicate DB Rows
+    #Eliminate all duplicate DB Rows and all Basic Lands
     for row in Card.objects.all():
         if Card.objects.filter(image_url=row.image_url).count() > 1:
             row.delete()
@@ -76,3 +90,11 @@ def addCardsToDatabase(set_data):
         elif row.card_name == "Forest":
             row.delete()
     return
+    
+def generatePack(given_set_name):
+    givenSet = Card.objects.filter(set_abbrev=given_set_name)
+    pack = []
+    for i in range(0,15):
+        pack.append(random.choice(givenSet))
+    return pack
+
