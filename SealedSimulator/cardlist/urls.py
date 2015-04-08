23f719a5d1773a4ca46f95b9e6ac69a7f343122f
Grant Carroll <gcarroll2603@gmail.com>
2015-04-08 17:15:59 -0400

    Deleted commented out code from previous branch

--- SealedSimulator/cardlist/urls.py
+++ SealedSimulator/cardlist/urls.py
@@ -5,5 +5,4 @@ urlpatterns = patterns('',
     url(r'^$', views.pack_select, name='pack select'),
     url(r'^openpacks/$', views.open_packs, name='open packs'),
     url(r'^printcards/$', views.print_selected_cards, name='print cards'),
-    #url(r'^importset/(?P<set_name>\w+)/$', views.import_set, name='import set'),
 )
